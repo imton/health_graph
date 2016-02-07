c08f42bd6df54c9687529b8df143ef2b8cba37c1
Gaston M <gaston@black.uy>
2016-02-07 01:34:33 -0300

    hola

---
+++ lib/health_graph/models/fitness_activity.rb
@@ -0,0 +1,26 @@
+module HealthGraph
+  class FitnessActivity
+    include Model
+    
+    hash_attr_accessor :uri, :userID, :type, :start_time, :total_distance, :distance, :duration, :heart_rate, :calories, :total_climb, :path, :source, 
+    
+    # class Path
+    #   include Model      
+      
+    #   # hash_attr_accessor :type, :start_time, :total_distance, :duration, :uri
+      
+    #   def initialize(hash) 
+    #     populate_from_hash! hash
+    #   end
+    # end
+                      
+    def initialize(access_token, path, params = {})            
+      self.access_token = access_token
+      response = get path, HealthGraph.accept_headers[:fitness_activity], params
+      self.body = response.body
+      populate_from_hash! self.body
+    end                           
+  end
+end
+
+# application/vnd.com.runkeeper.FitnessActivity+json
