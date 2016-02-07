e2452d4d2bfa7a04c5f0878be6fe514d891dccf1
Gaston M <gaston@black.uy>
2016-02-07 03:03:31 -0300

    hola

--- lib/health_graph/models/fitness_activity.rb
+++ lib/health_graph/models/fitness_activity.rb
@@ -17,6 +17,7 @@ module HealthGraph
     def initialize(access_token, path, params = {})
       self.access_token = access_token
       response = get path, HealthGraph.accept_headers[:fitness_activity], params
+      debugger
       self.body = response.body
       populate_from_hash! self.body
     end                           
